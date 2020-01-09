089e2835b418715126db88365a12674345ba1144
Raymond Chan <raymondcja@gmail.com>
2020-01-09 17:05:30 +1000

    hidden mongoDB password from git

--- NODE_PASSPORT_LOGIN/config/keys.js
+++ NODE_PASSPORT_LOGIN/config/keys.js
@@ -1,3 +1,5 @@
+const password = require('./MyData.js').password22;
+
 module.exports = {
-    mongoURI: 'mongodb+srv://pal:pal123@cluster0-r7sh7.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://pal:' + password + '@cluster0-r7sh7.mongodb.net/test?retryWrites=true&w=majority'
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # facebook-clone
 This is a facebook clone for me to personally test out my webdev skills that I have learnt so far. 
+
+current goals:
+- host this online (instead of localhost)
+- allow user to change/add profile details in dashboard
